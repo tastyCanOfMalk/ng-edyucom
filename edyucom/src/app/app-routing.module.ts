42d37f63eff53e51dd833837e87e0ebefa40ca62
Ed Yu <eddytheflow@gmail.com>
2023-08-22 17:11:35 -0400

    project modals working

--- edyucom/src/app/app-routing.module.ts
+++ edyucom/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SinglePageHomeComponent } from './single-page-home/single-page-home.component';
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
 
 const routes: Routes = [
   {path: '', component: SinglePageHomeComponent},
+  {path: 'projects/:title', component: ProjectDetailsComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
